101288d95bd84fae19fe13cf65472847c21ce4c8
Jason Bierque <bierquejason@gmail.com>
2018-07-07 08:17:04 +0200

    Create pyke.js

---
+++ libraries/javascript/pyke.js
@@ -0,0 +1,22 @@
+{
+    "owner": "systeme-cardinal / BierqueJason",
+    "repo": "Pyke",
+    "language": "JavaScript",
+    "links": [
+        {
+            "name": "npm",
+            "url": "https://www.npmjs.com/package/pyke"
+        },
+        {
+            "name": "rate-limiter-credits",
+            "url": "https://github.com/Colorfulstan/RiotRateLimiter-node"
+        }
+    ],
+    "metadata": {
+        "version": "1.0.5",
+    },
+    "tags": [
+        "v3",
+        "ddragon"
+    ]
+}
