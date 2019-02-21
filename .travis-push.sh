cded78ab8653860c9d0e3dae09dcb0b9c031c786
James <jglenn1@gmail.com>
2019-02-20 21:04:28 -0600

    Feature/docs (#97)
    * add sphinx configs
    
    * change theme
    
    * change theme
    
    * change theme
    
    * docs stuff
    
    * travis update node
    
    * travis stuff
    
    * travis stuff
    
    * travis stuff
    
    * travis stuff
    
    * travis stuff
    
    * travis stuff
    
    * travis stuff
    
    * Update .travis-push.sh
    
    * Update .travis-push.sh
    
    * travis stuff
    
    * update package.json
    
    * Update .travis.yml

---
+++ .travis-push.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+}
+
+commit_libraries_rst() {
+  git fetch
+  # Stash changes
+  git stash
+  git checkout master
+  # Unstash
+  git stash apply
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add -f docs/libraries.rst
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://wxwatch:${GITHUB_TOKEN}@github.com/wxwatch/riot-api-libraries.git > /dev/null 2>&1
+  git push origin master --quiet
+}
+
+setup_git
+commit_libraries_rst
+upload_files
---
+++ docs.js
@@ -0,0 +1,127 @@
+const fs = require('fs');
+const dateformat = require('dateformat');
+const { concatJsonFiles, resolveRepos } = require('./utilities');
+
+const execute = async () => {
+    console.log('Starting...');
+    const configs = await concatJsonFiles('libraries');
+    const data = await resolveRepos(configs);
+    const reposByLanguage = extractGroups(data);
+    const languageNames = Object.keys(reposByLanguage).sort();
+
+    let libraryString = `Riot API Libraries
+==========================
+
+`;
+
+    if (!languageNames) {
+        console.log('Cannot find language names');
+        return;
+    }
+
+    for (const name of languageNames) {
+        const index = languageNames.indexOf(name);
+        const reposInLanguage = reposByLanguage[name];
+        const repos = reposInLanguage.sort((a, b) => {
+            return +b.remote.updated_at - +a.remote.updated_at;
+        });
+
+        if (!containsValidLibrary(repos)) {
+            console.log(`${name} does not have any v3-supported repos`);
+            continue;
+        }
+
+        libraryString += `${name}
+------------------------------------------
+
+`;
+
+        if (!repos) {
+            console.log(`${name} contains no repos`);
+            continue;
+        }
+
+        for (const repo of repos) {
+            if (!repo.config.tags || repo.config.tags.indexOf('v4') < 0) {
+                console.log(`${repo.remote.owner}/${repo.remote.name} does not support v4`);
+                continue;
+            }
+
+            libraryString += `\`${repo.remote.name} <${repo.remote.url}>\`_ (☆ ${repo.remote.star_count})
+${"~".repeat(100)}
+
+${repo.config.description || repo.remote.description}
+
+`;
+
+            if (repo.config.links) {
+                libraryString += `Links
+::::::::::::::::
+`;
+                for (const link of repo.config.links) {
+                    const linkIndex = repo.config.links.indexOf(link);
+                    libraryString += `
+- \`${link.name} <${link.url}>\`_`;
+                }
+
+                libraryString += `
+`;
+            }
+
+            if (repo.remote.license) {
+                libraryString += `| License: \`${repo.remote.license.spdx_id} <http://choosealicense.com/licenses/${repo.remote.license.key}>\`_
+`;
+            } else {
+                libraryString += `| License: None
+`;
+            }
+
+            if (repo.config.tags) {
+                libraryString += `| Tags: `;
+                for (const tag of repo.config.tags) {
+                    const tagIndex = repo.config.tags.indexOf(tag);
+                    libraryString += tag;
+                    if (tagIndex !== repo.config.tags.length - 1) {
+                        libraryString += ', ';
+                    }
+                }
+                libraryString += `
+`;
+            }
+            libraryString += `| Last Updated: ${dateformat(repo.remote.updated_at, 'mmm d, yyyy')}
+
+-----------------
+
+`;
+        }
+    }
+
+    writeToFile(libraryString);
+    console.log('Successfully Updated Article');
+}
+
+const writeToFile = (text) => {
+    fs.writeFileSync('docs/libraries.rst', text);
+}
+
+const containsValidLibrary = (repos) => {
+    for (const repo of repos) {
+        if (repo.config.tags && repo.config.tags.indexOf('v3') > -1) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+const extractGroups = (data) => {
+    const groupToValues = data.reduce((obj, library) => {
+        obj[library.config.language] = obj[library.config.language] || [];
+        obj[library.config.language].push(library);
+        return obj;
+    }, {});
+
+    return groupToValues;
+};
+
+execute();
