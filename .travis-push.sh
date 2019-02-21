fe22850b547f9c5537d16818f78a596982fee686
James Glenn <jglenn1@gmail.com>
2019-02-20 20:44:22 -0600

    travis stuff

--- .travis-push.sh
+++ .travis-push.sh
@@ -9,6 +9,7 @@ setup_git() {
 commit_libraries_rst() {
   git remote update
   git fetch
+  git branch
   git checkout master
   # Current month and year, e.g: Apr 2018
   dateAndMonth=`date "+%b %Y"`
